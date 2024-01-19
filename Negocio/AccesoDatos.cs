c4e9db2acd492c9ac396aa15b1f9588a353f0158
LeandroIbaceta <72288170+Leandro-Ibaceta@users.noreply.github.com>
2024-01-19 18:07:55 -0300

    Agregado clases a los proyectos. Falta rellenarlos

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class AccesoDatos
+    {
+        //Acceso centralizado a datos de la DB
+    }
+}
---
+++ Negocio/ArticuloNegocio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class ArticuloNegocio
+    {
+        //De aca se sacan los datos de los articulos de la DB
+    }
+}
